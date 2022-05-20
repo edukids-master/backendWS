808976c3bdcac1640ea3fd15efd2389a2c09ef3c
Andriatiana Samyen Lee <Sam25v14@gmail.com>
2022-05-20 16:28:37 +0300

    generate quiz

--- routes/quiz.routes.js
+++ routes/quiz.routes.js
@@ -3,10 +3,20 @@ var router = express.Router();
 const Quiz = require("../models/quiz");
 
 router.get("/", function (req, res, next) {
-    Quiz.findOne((err, data) => {
-      if(err) return next(err)
+  Quiz.findOne((err, data) => {
+    if (err) return next(err);
 
-      res.json(data)
+    res.json(data);
+  });
+});
+
+router.get("/generate", function (req, res, next) {
+  const count = req.query.count;
+
+  Quiz.find({}, null, { limit: count }, (err, data) => {
+    if (err) return next(err);
+
+    res.json(data);
   })
 });
 
