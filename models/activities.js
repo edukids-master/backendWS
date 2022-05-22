928f1e3a25120cdeba4ca3ec6469572879ed792e
Andriatiana Samyen Lee <Sam25v14@gmail.com>
2022-05-22 09:18:26 +0300

    Update activities.js

--- models/activities.js
+++ models/activities.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const ActivitiesSchema = new mongoose.Schema({
     categorie: String,
     img: String,
-    titre: String
+    titre: String,
+    colors: []
 })
 
 module.exports = mongoose.model("Activities", ActivitiesSchema, "activities");
